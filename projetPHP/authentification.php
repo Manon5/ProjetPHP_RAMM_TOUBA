77d8c9a46ba187c9ebe3437a6f3fbc9b7f39add3
Manon5 <manon.touba05@gmail.com>
2019-10-18 08:41:15 +0200

    Update authentification.php

--- projetPHP/authentification.php
+++ projetPHP/authentification.php
@@ -15,7 +15,7 @@
         echo("Vous etes connecté !");
         session_start();
         $_SESSION['login'] = $id;
-        header('Location: menu_redacteur.php');
+        header('Location: page_redacteur.php');
       }else{
       echo("Votre identifiant est incorrect");
       }
@@ -24,7 +24,8 @@
 
     <h1>Page de connexion </h1>
 
-    <form action="authentification.php" method="post">
+    <form action="authentification.php" metmenu vers une page de rédaction permettant de taper
+le sujet du nouveau blog et le texte du créateur du nouveau blog. Lahod="post">
       <p>Login : <input type="text" name="log" /></p>
       <p>Mot de passe : <input type="password" name="mdp" /></p>
       <p><input type="submit" value="Se connecter"></p>
