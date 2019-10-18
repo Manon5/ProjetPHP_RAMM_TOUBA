e35a66c43d132c897e3d8310fc0595ea93377858
Manon5 <manon.touba05@gmail.com>
2019-10-18 09:39:15 +0200

    fini page de connexion du rédacteur

--- projetPHP/authentification.php
+++ projetPHP/authentification.php
@@ -7,25 +7,36 @@
   <body>
 
     <?php
+    include 'connexion.php';
+     $co = new Connexion();
+
+
     if($_SERVER['REQUEST_METHOD'] == "POST"){
       $id = $_POST["log"];
       $mdp = $_POST["mdp"];
 
-      if($id == "touba8u" & $mdp == "toto"){
-        echo("Vous etes connecté !");
-        session_start();
-        $_SESSION['login'] = $id;
-        header('Location: page_redacteur.php');
-      }else{
-      echo("Votre identifiant est incorrect");
+      $query = $co->creer_Connexion()->query("SELECT pseudo, motdepasse FROM redacteur");
+      $resultat = $query->fetchAll();
+      $valide = 0;
+      foreach ($resultat as $key => $variable){
+        if($id == $resultat[$key]['pseudo'] & $mdp == $resultat[$key]['motdepasse']){
+          echo("Vous etes connecté !");
+          session_start();
+          $_SESSION['login'] = $id;
+          header('Location: page_redacteur.php');
+          $valide = 1;
+        }
+      }
+
+      if($valide == 0){
+        echo("Mot de passe incorrect");
       }
     }
    ?>
 
     <h1>Page de connexion </h1>
 
-    <form action="authentification.php" metmenu vers une page de rédaction permettant de taper
-le sujet du nouveau blog et le texte du créateur du nouveau blog. Lahod="post">
+    <form action="authentification.php" method="post">
       <p>Login : <input type="text" name="log" /></p>
       <p>Mot de passe : <input type="password" name="mdp" /></p>
       <p><input type="submit" value="Se connecter"></p>
