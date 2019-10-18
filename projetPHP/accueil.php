090826b5aa7534ad3123af09e0a97b47f9fa22ea
HamaraH <50440969+HamaraH@users.noreply.github.com>
2019-10-18 09:00:11 +0200

    Add files via upload
    Mise à jour de l'accueil

--- projetPHP/accueil.php
+++ projetPHP/accueil.php
@@ -7,7 +7,16 @@
     <title> Blog pour le projet de PHP </title>
   </head>
 
+<div>
+  <h1> Liste des sujets du blog : </h1>
+</div>
 
+<div>
+  <form action="creationCompte.php">
+    <input type="submit" value="Créer un compte rédacteur" />
+</form>
+
+</div>
 
 </body>
 
