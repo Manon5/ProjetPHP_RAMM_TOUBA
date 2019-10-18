71d808ac07f512bc6840378e5c94233597c82580
HamaraH <50440969+HamaraH@users.noreply.github.com>
2019-10-18 10:38:23 +0200

    Add files via upload
    Fin de la page pour créer un compte

--- projetPHP/creationCompte.php
+++ projetPHP/creationCompte.php
@@ -2,6 +2,11 @@
 <html>
 
 <head>
+
+  <?php
+    include('connexion.php');
+   ?>
+
   <title> Création d'un compte </title>
   <head>
 
@@ -52,11 +57,13 @@
 
   </form>
 
-<!--Partie traitement des données-->
+<!-- Partie traitement des données -->
 
-  <?php
+<?php
 
-  if(isSet($_POST['nom'])){
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+    $formValide = false;  //Booleen qui verifie si le formulaire est valide
 
     if ($_POST['nom']=="")
       echo("<div id='alerteForm'> Veuillez saisir un nom </div>");
@@ -72,10 +79,48 @@
 
     else if ($_POST['pseudo']=="")
         echo("<div id='alerteForm'> Veuillez saisir un pseudo </div>");
+    else
+        $formValide = true;       //Si on arrive ici, le formulaire est valide
 
+    if ($formValide == true){     //Pour eviter que cette partie s'execute au chargement de la page
 
+      $maConnexion = new Connexion();
+      $objetPDO = $maConnexion->creer_Connexion();
+      $statement = $objetPDO->query("SELECT pseudo FROM redacteur"); //On prend la liste des pseudos dans la bdd
+
+      $pseudoOk = true; //Booleen qui sert à voir si le pseudo est valide
+
+     while ($colonne = $statement->fetch()) {
+
+        if($colonne['pseudo'] == $_POST['pseudo']){
+
+            echo("<div> Ce pseudo est déjà utilisé, veuillez en saisir un autre");
+            $pseudoOk=false;    //Si on trouve le pseudo dans la bdd, on ne fait rien
+            break;
+
+        }
+
+      }
+
+    if($pseudoOk == true){  //Si le pseudo n'est pas trouvé dans la bdd
+
+          $statement = $objetPDO->prepare("INSERT INTO redacteur(nom, prenom, adressemail, motdepasse, pseudo) VALUES(?,?,?,?,?)");
+
+          $statement->bindValue(1, $_POST['nom']);
+          $statement->bindValue(2, $_POST['prenom']);
+          $statement->bindValue(3, $_POST['adresse']);
+          $statement->bindValue(4, $_POST['mdp']);
+          $statement->bindValue(5, $_POST['pseudo']);
+
+          $statement->execute();           //On l'insère avec les paramètres du form
+          header("Location:accueil.php");  //Et on redirige vers l'accueil
+      }
 
     }
+
+}
+
+
 
     ?>
 
