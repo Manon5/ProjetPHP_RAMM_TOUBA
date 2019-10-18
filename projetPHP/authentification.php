823088c731cd3e39b2e1cbbe4fad88acaacf2a85
Manon5 <manon.touba05@gmail.com>
2019-10-18 08:39:31 +0200

    ajout du code php sur la page de connexion

--- projetPHP/authentification.php
+++ projetPHP/authentification.php
@@ -6,11 +6,27 @@
 
   <body>
 
+    <?php
+    if($_SERVER['REQUEST_METHOD'] == "POST"){
+      $id = $_POST["log"];
+      $mdp = $_POST["mdp"];
+
+      if($id == "touba8u" & $mdp == "toto"){
+        echo("Vous etes connecté !");
+        session_start();
+        $_SESSION['login'] = $id;
+        header('Location: menu_redacteur.php');
+      }else{
+      echo("Votre identifiant est incorrect");
+      }
+    }
+   ?>
+
     <h1>Page de connexion </h1>
-    
+
     <form action="authentification.php" method="post">
       <p>Login : <input type="text" name="log" /></p>
-      <p>Mot de passe : <input type="text" name="mdp" /></p>
+      <p>Mot de passe : <input type="password" name="mdp" /></p>
       <p><input type="submit" value="Se connecter"></p>
     </form>
   </body>
