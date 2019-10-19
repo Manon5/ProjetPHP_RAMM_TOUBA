f11fbb792da75be54b61690da4a903e3cf8dc0ad
HamaraH <50440969+HamaraH@users.noreply.github.com>
2019-10-19 20:32:31 +0200

    Add files via upload
    Ajout des articles sur la page d'accueil

--- projetPHP/accueil.php
+++ projetPHP/accueil.php
@@ -30,35 +30,53 @@
 
 </div>
 
-<div id="articles">
-    <form action="listeArticles.php">
-      <input type="submit" value="Voir les articles publics">
-    </form>
-</div>
+
 
 <h1 class="titreAccueil"> Voici les derniers articles disponibles sur le blog : </h1>
 
 
   <?php
 
+
+  session_start();
+
   $maConnexion = new Connexion();
   $objetPDO = $maConnexion->creer_Connexion();
-  $statement = $objetPDO->query("SELECT titresujet, textesujet FROM sujet ORDER BY  datesujet DESC");
+  $statement = $objetPDO->query("SELECT titresujet, textesujet, datesujet, nom, prenom FROM sujet,redacteur WHERE sujet.idredacteur = redacteur.idredacteur ORDER BY  datesujet DESC");
 
+  echo("<div id='articlesPresentation'>");
 
-   while ($colonne = $statement->fetch()){
+  $nbArticles=0;
+
+   while ( ($colonne = $statement->fetch()) && ($nbArticles < 2)  ){
+
+    ++$nbArticles;
 
     echo(
 
-       "<div class='titreArticle'>"   . $colonne['titresujet'] . "</div>"
-      ."<div class='article'>"   . $colonne['textesujet'] . "</div>
+       "<div class='article'>" .
+
+          "<h2 class='titreArticle'>"   . $colonne['titresujet'] . "</h2>"    .
+          "<div class='contenuArticle'>"   . $colonne['textesujet'] . "</div> <br />
+           <div class  = 'dateArticle'> Ecrit par : " . $colonne['prenom'] . " ".  $colonne['nom'] . ", le :  <b>" .  $colonne['datesujet'] . " </b> </div>
+      </div>.
+
+
        <br/ >"
+
     );
 
-   }
+  }
 
+  echo("</div>");
 
    ?>
+
+   <div id="articles">
+       <form action="listeArticles.php">
+         <input type="submit" value="Voir tous les articles disponibles">
+       </form>
+   </div>
 
 </body>
 
