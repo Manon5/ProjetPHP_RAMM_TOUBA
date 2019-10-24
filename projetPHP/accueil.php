f4cc0758f59f1ca5b56361dcfeb4c85b262f7f59
HamaraH <50440969+HamaraH@users.noreply.github.com>
2019-10-24 11:35:01 +0200

    Add files via upload
    Indentement

--- projetPHP/accueil.php
+++ projetPHP/accueil.php
@@ -4,103 +4,95 @@
 
   <head>
 
-    <?php
-    session_start();
-
-    include 'connexion.php';
-     ?>
-    <meta charset = "UTF-8">
     <title> Blog pour le projet de PHP </title>
+
     <link rel = "stylesheet"
-     type = "text/css"
-     href = "style.css"/>
+          type = "text/css"
+          href = "style.css"/>
+
+     <meta charset = "UTF-8">
+
+     <?php
+        session_start();
+        include 'connexion.php';
+     ?>
 
   </head>
 
 <body>
 
-    <nav class="menu">
-
+    <nav class="menu">    <!-- Menu du haut qui est adapté en fonction de si la personne est connectée ou non -->
       <ul>
-            <li> <a href="listeArticles.php">Voir tous les articles</a> </li>
+            <li> <a href="listeArticles.php">Voir tous les articles </a> </li>
 
             <?php
 
             if(isSet($_SESSION['pseudo'])){
 
-              echo(' <li class = "itemMenu"> <a href="page_redacteur.php"> Ecrire un article </a> </li>
+              echo(' <li class = "itemMenu"> <a href="page_redacteur.php"> Ecrire un article </a> </li>');
+              echo(' <li class="itemMenu"> <a href="liste_articles_perso.php" class="itemMenu"> Voir vos articles </a> </li>');
 
-                  <li class="itemMenu"> <a href="liste_articles_perso.php" class="itemMenu"> Voir vos articles </a> </li>'
-                );
             }
 
-            ?>
+            echo(' </ul>
+                  </nav>');
 
-      </ul>
+            if(!isSet($_SESSION['pseudo'])){
 
-    </nav>
+              echo("<h1 class='titre'> Bienvenue sur le blog du projet PHP ! </h1>");
 
+              echo('<div id="menuAccueil">');
 
+                echo('<form action="creationCompte.php">');
+                  echo('<input type="submit" value="Créer un compte rédacteur">');
+                echo('</form>');
 
+                echo('<form action="authentification.php">');
+                  echo('<input type="submit" value="S\'authentifier">');
+                echo('</form>');
 
-
-<?php
-
-
-
-  if(!isSet($_SESSION['pseudo'])){
-
-    echo("<h1 class='titre'> Bienvenue sur le blog du projet PHP ! </h1>");
-
-      echo('<div id="menuAccueil">');
-      echo('<form action="creationCompte.php">');
-          echo('<input type="submit" value="Créer un compte rédacteur">');
-      echo('</form>');
-
-      echo('<form action="authentification.php">');
-        echo('<input type="submit" value="S\'authentifier">');
-      echo('</form>');
+              echo("</div>");
 
   }
 
-  else{
+            else{
 
-      echo("<h1 class='titre'> Bienvenue sur le blog du projet PHP, " . $_SESSION['pseudo'] ." ! </h1>");
+                echo("<h1 class='titre'> Bienvenue sur le blog du projet PHP, " . $_SESSION['pseudo'] ." ! </h1>");
 
-        echo('<div id="menuAccueil">');
-    echo("<form action='deconnexion.php'>");
-      echo("<input type='submit' value='Se déconnecter'>");
-    echo("</form>");
+                echo('<div id="menuAccueil">');
+                  echo("<form action='deconnexion.php'>");
+                    echo("<input type='submit' value='Se déconnecter'>");
+                  echo("</form>");
+                echo("</div>");
 
-  }
-
-echo("</div>");
+            }
 
 ?>
 
 <h1 class="titre"> Voici les derniers articles disponibles sur le blog : </h1>
 
-
   <?php
-
-
-
 
   $maConnexion = new Connexion();
   $objetPDO = $maConnexion->creer_Connexion();
-  $statement = $objetPDO->query("SELECT titresujet, textesujet, datesujet, nom, prenom, idsujet FROM sujet,redacteur WHERE sujet.idredacteur = redacteur.idredacteur ORDER BY  idsujet DESC");
+  $statement = $objetPDO->query("SELECT titresujet, textesujet, datesujet, nom, prenom, idsujet
+                                 FROM sujet,redacteur
+                                 WHERE sujet.idredacteur = redacteur.idredacteur
+                                 ORDER BY  idsujet DESC");   //Requete qui récupère les infos des articles et du rédacteur concerné
 
-  echo("<div id='articlesPresentation'>");
+  echo("<div id='articlesPresentation'>");  //Div qui contient les 4 articles de présentation sur la page d'accueil
 
   $nbArticles=0;
 
-   while ( ($colonne = $statement->fetch()) && ($nbArticles < 4)  ){
+   while ( ($colonne = $statement->fetch()) && ($nbArticles < 4)  ){  //Tant qu'on a des résultats et qu'on a publié moins de 4 articles (histoire de ne pas surchager l'accueil)
 
     ++$nbArticles;
 
-    if (strlen($colonne['textesujet']) > 400){
+    if (strlen($colonne['textesujet']) > 400){    //Si le contenu de l'article est trop grand, on le coupe pour inciter à aller voir l'article en entier
+
       $contenu = $colonne['textesujet'] ;
       $contenu = substr($contenu,0,399) . "...";
+
     }
 
     else
@@ -109,17 +101,20 @@ echo("</div>");
       echo('<div class="article">');
 
         echo('<h2 class="titreArticle">'   . $colonne['titresujet'] . '</h2>');
+
         echo('<div class="contenuArticle">'   . $contenu . '</div>');
-        echo(' <br />');
+          echo(' <br />');
 
-      echo('<div class  = "infosArticle">');
-          echo('<div class="lienReponses">');
-            echo(" <a href='blog.php?idsujet="  . $colonne['idsujet'] . "'> Lire l'article  </a>");
-          echo('</div>');
+          echo('<div class  = "infosArticle">');
 
-          echo('<div class="dateArticle"> Ecrit par : ' . $colonne['prenom'] . " ".  $colonne['nom'] . ", le :  <b>" .  $colonne['datesujet'] . '  </b>  </div>');
+            echo('<div class="lienReponses">');
+              echo(" <a href='blog.php?idsujet="  . $colonne['idsujet'] . "'> Lire l'article  </a>");  //Lien qui affiche l'article et ses réponses
+            echo('</div>');
 
-      echo('</div>');
+           setlocale(LC_TIME, 'fr', 'fr_FR', 'fr_FR.ISO8859-1'); //Changer la timezone pour formater les dates en français
+           echo('<div class="dateArticle"> Ecrit par <b>' . $colonne['prenom'] . " ".  $colonne['nom'] . "</b>, le <b>" . strftime("%A %d %B %G",strtotime($colonne['datesujet']))  . '  </b>  </div>');
+
+         echo('</div>');
 
       echo('<br />');
 
@@ -128,9 +123,10 @@ echo("</div>");
 
   echo("</div>");
 
-  unset($objetPDO);
+  unset($objetPDO); //Fermeture de la connexion
+
    ?>
 
-</body>
+ </body>
 
 </html>
