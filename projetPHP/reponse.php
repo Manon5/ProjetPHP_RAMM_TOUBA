9826ccc333c58f36c946f3ad484eb615366d2ede
Manon5 <manon.touba05@gmail.com>
2019-10-24 08:30:38 +0200

    ajout barre de navigation dans réponse

--- projetPHP/reponse.php
+++ projetPHP/reponse.php
@@ -8,10 +8,36 @@
     include('connexion.php');
    ?>
 
+
+
   <title> Rédaction d'une réponse </title>
+  <link rel = "stylesheet"
+   type = "text/css"
+   href = "styleAccueilListeArticles.css"/>
   <head>
 
 <body>
+
+  <nav class="menu">
+
+    <ul>
+          <li> <a href="accueil.php">Revenir à l'accueil</a> </li>
+          <li> <a href="listeArticles.php">Voir tous les articles</a> </li>
+
+          <?php
+
+          if(isSet($_SESSION['pseudo'])){
+
+            echo(' <li class = "itemMenu"> <a href="page_redacteur.php"> Ecrire un article </a> </li>
+            <li class="itemMenu"> <a href="liste_articles_perso.php" class="itemMenu"> Voir vos articles </a> </li>');                  echo("<li class='itemMenu'> <a href='javascript:if(confirm(\"Vous allez être déconnecté\")){   location.href=\"deconnexion.php\" }
+            ' > Se déconnecter </a> </li>");
+          }
+
+          ?>
+
+    </ul>
+
+  </nav>
 
   <h1>Rédaction d'une réponse : </h1>
 
