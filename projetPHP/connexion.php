11e0e136f1ff12ed174fef7b5fda059d74078ffd
HamaraH <50440969+HamaraH@users.noreply.github.com>
2019-10-28 18:01:15 +0100

    Update connexion.php

--- projetPHP/connexion.php
+++ projetPHP/connexion.php
@@ -9,8 +9,8 @@ class Connexion{
   public function __construct(){
 
     $this->url = 'mysql:host=devbdd.iutmetz.univ-lorraine.fr;dbname=ramm8u_projetPHP;port=3306' ;
-    $this->login = 'ramm8u_appli' ;     //Inserez ici votre login
-    $this->password = 'f8fXf8bXf8fN' ; //Inserez ici votre mdp
+    $this->login = '' ;     //Inserez ici votre login
+    $this->password = '' ; //Inserez ici votre mdp
 
   }
 
